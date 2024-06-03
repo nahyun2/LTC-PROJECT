ec5317c3e7169a99a635b76d472772e13fcc86fe
yeontteo <151910330+yeontteo@users.noreply.github.com>
2024-06-04 05:19:40 +0900

    Update LTC_TrashMain.py

--- Trash/LTC_TrashMain.py
+++ Trash/LTC_TrashMain.py
@@ -1,21 +1,31 @@
 import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import Qt
 
-class TrashMain_window(QWidget):
+class TrashMainWindow(QWidget):
     def __init__(self):
-        super(TrashMain_window, self).__init__()
-        self.setWindowTitle("Living Tips for CBNU")
+        super().__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle("Living Tips for CBNU - Trash Tips")
         self.setGeometry(400, 100, 1180, 820)
         
-        self.image_label_Trashmain = QLabel(self)
-        self.image_label_Trashmain.setGeometry(0, 0, 1180, 820)    
-        pixmap_Trashmain = QPixmap('background.jpg')
-        scaled_pixmap_Trashmain = pixmap_Trashmain.scaled(self.image_label_Trashmain.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
-        self.image_label_Trashmain.setPixmap(scaled_pixmap_Trashmain)
-        self.image_label_Trashmain.setAlignment(Qt.AlignCenter)
+        layout = QVBoxLayout()
+    
+        self.image_label_trashmain = QLabel(self)
+        self.image_label_trashmain.setGeometry(0, 0, 1180, 820)    
+        pixmap_trashmain = QPixmap('background.jpg')
+        scaled_pixmap_trashmain = pixmap_trashmain.scaled(self.image_label_trashmain.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
+        self.image_label_trashmain.setPixmap(scaled_pixmap_trashmain)
+        self.image_label_trashmain.setAlignment(Qt.AlignCenter)
 
+        layout.addWidget(self.image_label_trashmain)
+        
+        self.setLayout(layout)
+        self.show()
 
     def create_image_button(self, image_path, x, y, tooltip, width, height):
         btn = QPushButton(parent=self)
@@ -26,9 +36,9 @@ class TrashMain_window(QWidget):
         btn.setToolTip(tooltip)
         btn.setGeometry(x, y, scaled_pixmap.width(), scaled_pixmap.height())
         btn.setStyleSheet("border: none;")
-        
 
-app_Trashmain = QApplication(sys.argv)
-Trashmainwindow = TrashMain_window()
-Trashmainwindow.show()
-app_Trashmain.exec_()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    trashmainwindow = TrashMainWindow()
+    trashmainwindow.show()
+    sys.exit(app.exec_())
