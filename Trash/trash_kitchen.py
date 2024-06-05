8a0e7306650b09c6744b938b8f2c7a4c58ddd5b9
jaeyeo0ng <165654054+jaeyeo0ng@users.noreply.github.com>
2024-06-05 23:49:24 +0900

    Update trash_kitchen.py

--- Trash/trash_kitchen.py
+++ Trash/trash_kitchen.py
@@ -79,6 +79,12 @@ class ImageBackgroundApp(QWidget):
         self.image_label = QLabel(self)
         self.image_label.hide()
 
+        self.image_title_label = QLabel(self)  # 추가된 부분
+        self.image_title_label.setGeometry(100, 680, 400, 50)  # 사진 아래에 제목을 표시할 위치
+        self.image_title_label.setFont(QFont('Arial', 22, QFont.Bold))
+        self.image_title_label.setAlignment(Qt.AlignCenter)
+        self.image_title_label.hide()
+
         self.additional_image_label = QLabel(self)
         self.additional_image_label.setPixmap(QPixmap(os.path.join(self.base_path, 'memo.png')))
         if self.additional_image_label.pixmap().isNull():
@@ -120,73 +126,94 @@ class ImageBackgroundApp(QWidget):
             os.path.join(self.kitchen_path, 'knife.png')
         ]
 
+        image_titles = ['냄비', '가위', '장갑', '그릇', '식기', '식칼']  # 추가된 부분
+
         image_sizes = [(400, 400), (400, 400), (400, 400), (400, 400), (400, 400), (400, 400)]
 
         texts = [
-            ('처리 방법\n- 음식물이나 이물질이 묻어있지 않게 깨끗이 세척한 후 배출\n'
-             '- 유리 뚜껑/플라스틱 손잡이 등 분리할 수 있는 다른 재질이 있으면\n'
-             '떼어낸 후 재질별로 분리 배출, 따로 떼어낼 수 없으면 그대로 배출\n'
-             '- 캔류의 수거 장소가 종류에 따라 나뉘어져 있으면 캔류(철)로 분리배출하고,\n'
-             '그렇지 않으면 다른 고철들과 함께 캔류로 분리배출'),
+            ('처리 방법', 
+             '\n\n\n-음식물이나 이물질이 묻어있지 않게\n 깨끗이 세척한 후 배출\n'
+             '\n- 유리 뚜껑/플라스틱 손잡이 등 분리할 수\n 있는 다른 재질이 있으면'
+             '  떼어낸 후 재질\n 별로 분리 배출, 따로 떼어낼 수 없으면\n 그대로 배출\n'
+             '\n- 캔류의 수거 장소가 종류에 따라 나뉘어져\n 있으면 캔류(철)로 분리배출하고,'
+             '  그렇지\n 않으면 다른 고철들과 함께 캔류로 분리배출'),
 
-            ('처리 방법\n- 가위는 고철류지만 재활용 업체의 수거 과정에서 사람이 다칠 위험이\n'
-             '있기 때문에 고철이 아닌 일반쓰레기(종량제봉투)로 분류\n'
-             '- 가위의 날카로운 날이 종량제봉투를 찢고 나오는 등 사고가 일어나지 않도록\n'
-             '가위를 신문지로 여러 겹 싸고 테이프로 단단히 고정해서 종량제봉투에 배출'),
+            ('처리 방법', 
+             '\n\n\n- 가위는 고철류지만 재활용 업체의 수거\n 과정에서 사람이 다칠 위험이'
+             '  있기 때문에\n 고철이 아닌 일반쓰레기(종량제봉투)로 분류\n'
+             '\n- 가위의 날카로운 날이 종량제봉투를 찢고\n 나오는 등 사고가 일어나지 않도록'
+             '  가위를\n 신문지로 여러 겹 싸고 테이프로 단단히\n 고정해서 종량제봉투에 배출'),
 
-            ('처리 방법\n- 고무장갑, 라텍스장갑, 니트릴장갑 등 고무 재질은 재활용이 어려워서\n'
-             '분리수거되지 않으니 모두 일반쓰레기(종량제봉투)로 배출\n'
-             '- 천연고무는 재활용이 가능하나 우리가 사용하는 대부분의 고무 제품은\n'
-             '합성 고무 제품으로 재활용이 불가능'),
+            ('처리 방법', 
+             '\n\n\n- 고무장갑, 라텍스장갑, 니트릴장갑 등 고무\n 재질은 재활용이 어려워서'
+             '  분리수거되지\n 않으니 모두 일반쓰레기(종량제봉투)로 배출\n'
+             '\n- 천연고무는 재활용이 가능하나 우리가 사용\n하는 대부분의 고무 제품은'
+             '  합성 고무 제품\n으로 재활용이 불가능'),
 
-            ('처리 방법\n- 사기그릇이나 유리그릇은 재활용이 불가능하고 불에 타지 않는\n'
-             '쓰레기이므로 불연성 쓰레기 봉투(마대)에 배출\n'
-             '- 플라스틱 그릇은 깨끗이 세척 후 플라스틱으로 분리배출\n'
-             '- 유기그릇(방짜 유기, 스테인리스, 놋그릇 등)도 재활용이 가능하니\n'
-             '깨끗이 씻어서 캔류 (고철)로 분리배출'),
+            ('처리 방법', 
+             '\n\n\n- 사기그릇이나 유리그릇은 재활용이 불가능\n하고 불에 타지 않는'
+             '  쓰레기이므로 불연성\n 쓰레기 봉투(마대)에 배출\n'
+             '\n- 플라스틱 그릇은 깨끗이 세척 후 플라스틱\n으로 분리배출\n'
+             '\n- 유기그릇(방짜 유기, 스테인리스, 놋그릇 등)\n도 재활용이 가능하니'
+             '  깨끗이 씻어서 캔류\n (고철)로 분리배출'),
 
-            ('처리 방법\n- 깨끗이 씻어 캔류(철)로 분리배출\n'
-             '- 나무, 플라스틱 손잡이 등 다른 재질을 분리할 수 있는 경우 떼어낸 후\n'
-             '각각의 재질별로 분리배출, 분리가 어려우면 그대로 배출\n'
-             '- 나무젓가락 등 나무 재질의 식사 도구는 일반쓰레기(종량제봉투)\n'
-             '- 일회용 플라스틱 숟가락, 포크, 나이프는 플라스틱으로 분리배출'),
+            ('처리 방법', 
+             '\n\n\n- 깨끗이 씻어 캔류(철)로 분리배출\n'
+             '- 나무, 플라스틱 손잡이 등 다른 재질을 분리\n할 수 있는 경우 떼어낸 후'
+             '  각각의 재질별로\n 분리배출, 분리가 어려우면 그대로 배출\n'
+             '\n- 나무젓가락 등 나무 재질의 식사 도구는\n 일반쓰레기(종량제봉투)\n'
+             '\n- 일회용 플라스틱 숟가락, 포크, 나이프는\n 플라스틱으로 분리배출'),
 
-            ('처리 방법\n- 고철류지만 재활용 업체의 수거 과정에서 다칠 위험이 있기 때문에\n'
-             '고철이 아닌 일반쓰레기(종량제봉투)로 배출\n'
-             '- 날카로운 칼날이 종량제봉투를 찢고 나오는 등의 사고가 일어나지 않도록\n'
-             '칼을 신문지로 여러 겹 싸고 테이프로 단단히 고정해서 종량제봉투 배출')
+            ('처리 방법', 
+             '\n\n\n- 고철류지만 재활용 업체의 수거 과정에서\n 다칠 위험이 있기 때문에'
+             '  고철이 아닌 일반\n 쓰레기(종량제봉투)로 배출\n'
+             '\n- 날카로운 칼날이 종량제봉투를 찢고 나오는\n 등의 사고가 일어나지 않도록\n'
+             ' 칼을 신문지로 여러 겹 싸고 테이프로 단단히\n 고정해서 종량제봉투 배출')
         ]
 
-        self.image_label.setPixmap(QPixmap(image_paths[index]).scaled(image_sizes[index][0], image_sizes[index][1], Qt.KeepAspectRatio))
+        text_positions = [
+            [(610, 210), (620, 230)],
+            [(610, 210), (620, 230)],
+            [(610, 210), (620, 230)],
+            [(610, 210), (620, 230)],
+            [(610, 210), (620, 230)],
+            [(610, 210), (620, 230)]
+        ]
+
+        self.image_label.setPixmap(QPixmap(image_paths[index]).scaled(image_sizes[index][0], image_sizes[index][1], Qt.KeepAspectRatio, Qt.SmoothTransformation))
         if self.image_label.pixmap().isNull():
             print(f"Failed to load {image_paths[index]}")
         self.image_label.setGeometry(100, 270, image_sizes[index][0], image_sizes[index][1])
         self.image_label.show()
 
+        self.image_title_label.setText(image_titles[index])  # 추가된 부분
+        self.image_title_label.show()  # 추가된 부분
+
         self.additional_image_label.show()
 
         # 텍스트 라벨 갯수를 텍스트 갯수에 맞게 설정
-        for label in self.text_labels:
-            label.hide()
-        
-        self.text_labels = []
+        for i in range(len(self.text_labels)):
+            self.text_labels[i].hide()
 
-        text = texts[index]
-        text_label = QLabel(text, self)
-        text_label.setFont(QFont('Arial', 14, QFont.Bold))
-        text_label.setGeometry(560, 200, 560, 400)  # 크기와 위치 조정
-        text_label.setWordWrap(True)
-        text_label.show()
-        self.text_labels.append(text_label)
+        required_text_labels = len(texts[index])
+        while len(self.text_labels) < required_text_labels:
+            text_label = QLabel(self)
+            self.text_labels.append(text_label)
 
-        # 선택한 아이템의 이름을 왼쪽 아래에 표시
-        self.item_label.setText(self.labels[index].text())
-        self.item_label.show()
+        text_sizes = [(750, 50), (560, 400)]  # 각 텍스트 라벨의 크기 지정
+        text_fonts = [QFont('Arial', 25, QFont.Bold), QFont('Arial', 18)]  # 각 텍스트 라벨의 폰트 지정
+
+        for i, text in enumerate(texts[index]):
+            self.text_labels[i].setText(text)
+            self.text_labels[i].setFont(text_fonts[i])  # 폰트 지정
+            self.text_labels[i].setGeometry(text_positions[index][i][0], text_positions[index][i][1], text_sizes[i][0], text_sizes[i][1])  # 크기와 위치 조정
+            self.text_labels[i].show()
 
         self.close_button.show()
 
     def closeDetails(self):
         self.image_label.hide()
+        self.image_title_label.hide()  # 추가된 부분
         self.additional_image_label.hide()
         for text_label in self.text_labels:
             text_label.hide()
@@ -199,7 +226,6 @@ class ImageBackgroundApp(QWidget):
         self.title_label.show()
         self.main_button.show()
         self.previous_button.show()
-        self.item_label.hide()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
